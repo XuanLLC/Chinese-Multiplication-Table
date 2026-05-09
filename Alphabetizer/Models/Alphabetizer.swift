bf971edbf0dec215ad55996206a21ee9b26f2d3d
吴烜 <wuxuan.ecios@gmail.com>
2026-05-08 19:40:20 -0700

    积为何为8

--- Alphabetizer/Models/Alphabetizer.swift
+++ Alphabetizer/Models/Alphabetizer.swift
@@ -16,56 +16,37 @@ class Alphabetizer {
     
     /// Checks if tiles are in alphabetical order
     func 揭晓答案() {
-        // Check if the tiles are alphabetized
-        let userSortedTiles = tiles.sorted {
-            $0.position.x < $1.position.x
-        }
-        let alphabeticallySortedTiles = tiles.sorted {
-            $0.word.lexicographicallyPrecedes($1.word)
-        }
-        let isAlphabetized = userSortedTiles == alphabeticallySortedTiles
-
-        // If alphabetized, increment the score
-        if isAlphabetized {
-            score += 1
-        }
         
         // Update the message to win or lose
-        message = "333"
-        
-        // Flip over correct tiles
-        for (tile, correctTile) in zip(userSortedTiles, alphabeticallySortedTiles) {
-            let tileIsAlphabetized = tile == correctTile
-            tile.flipped = tileIsAlphabetized
-        }
-        
+        message = "自己算："
+        /*
         Task { @MainActor in
             // Delay 2 seconds
             try await Task.sleep(for: .seconds(2))
-            
+            */
             // If alphabetized, generate new tiles
+            开新局()
+            
             var 积 = 1
-            if isAlphabetized {
-                积 = 开新局()
-            }
-
             // Flip tiles back to words
             for tile in tiles {
                 tile.flipped = false
+                print(tile.数)
+                积 = 积 * (tile.数)
+                print("积："+String(积))
             }
 
             // 恢复指令 Display instructions
             message = String(积)
-        }
+        //}
         
     }
     // MARK: private implementation
 
     /// Updates `tiles` with a new set of unalphabetized words
-    private func 开新局() -> Int {
+    private func 开新局() {
         let 两数 = 数字.随机选取几个(count: 2)
         if tiles.isEmpty {
-            var 积 = 1
             for word in 两数 {
                 var 简体 = "一"
                 var 数 = 1
@@ -80,17 +61,12 @@ class Alphabetizer {
                     数 = 4
                 }
                 tiles.append(Tile(word: word, 简体: 简体, 数: 数))
-                积 = 积 * 数
             }
-            return 积
         } else {
             // Assign new words to existing tiles
-            var 积 = 1
             for (tile, word) in zip(tiles, 两数) {
                 tile.word = word
-                积 = 积 * tile.数
             }
-            return 积
         }
     }
 }
