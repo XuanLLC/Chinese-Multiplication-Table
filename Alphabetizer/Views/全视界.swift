d81a507101cd97e5c17f8a6ef19d1ac1dbafbb0f
吴烜 <wuxuan.ecios@gmail.com>
2026-05-16 10:58:20 -0700

    第二个tab

--- Alphabetizer/Views/全视界.swift
+++ Alphabetizer/Views/全视界.swift
@@ -1,17 +1,22 @@
+//
+//  ContentView.swift
+//  Alphabetizer
+//
+//  Created by Xuan Wu on 5/16/26.
+//
 import SwiftUI
 
 struct 全视界: View {
     var body: some View {
-        VStack(spacing: 15) {
-            积视界()
-            两卡视界()
-            下一个()
+        TabView {
+            视界1()
+                .tabItem {
+                    Label("", systemImage: "1.circle")
+                }
+            Text("二档")
+                .tabItem {
+                    Label("", systemImage: "2.circle")
+            }
         }
-        .padding(.top, 60)
     }
-}
-
-#Preview {
-    全视界()
-        .environment(乘法口诀())
 }
---
+++ Alphabetizer/Views/视界1.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct 视界1: View {
+    var body: some View {
+        VStack(spacing: 15) {
+            积视界()
+            两卡视界()
+            下一个()
+        }
+        .padding(.top, 60)
+    }
+}
+
+#Preview {
+    视界1()
+        .environment(乘法口诀())
+}
