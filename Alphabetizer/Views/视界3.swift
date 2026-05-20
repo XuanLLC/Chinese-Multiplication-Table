1b2c71b92543d9d9b65972dce6d329f10c997e0f
YUNBO WANG <3299835829@qq.com>
2026-05-20 16:17:42 +0800

    Implement ordered third tab practice

--- Alphabetizer/Views/视界3.swift
+++ Alphabetizer/Views/视界3.swift
@@ -8,16 +8,23 @@
 import SwiftUI
 
 struct 视界3: View {
+    @State private var 口诀 = 乘法口诀()
+
     var body: some View {
         VStack(spacing: 15) {
             积视界(中文: true)
-            两卡视界() // 待做：需按先小后大点击后通过
+            两卡视界 { 字卡 in
+                口诀.按先小后大点击(字卡)
+            }
         }
         .padding(.top, 60)
+        .environment(口诀)
+        .onAppear {
+            口诀.开新局(显示积: true, 随机顺序: true)
+        }
     }
 }
 
 #Preview {
-    视界1()
-        .environment(乘法口诀())
+    视界3()
 }
